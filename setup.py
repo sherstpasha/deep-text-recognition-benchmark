80425426da848504907963dcb4a7d7ce852c1ea9
Sherstpasha <sherstpasha99@gmail.com>
2025-04-26 19:18:28 +0700

    .

---
+++ setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup
+from pathlib import Path
+
+# читаем список зависимостей из requirements.txt
+here = Path(__file__).parent
+install_requires = (here / "requirements.txt").read_text().splitlines()
+
+setup(
+    name="hardocr",
+    version="0.1.1",
+    description="None",
+    install_requires=install_requires,
+)

b1654f613e264e3c88197fcdc0aa57b542cd409a
sherstpasha <sherstpasha99@gmail.com>
2025-07-12 17:43:09 +0700

    0.1.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = (here / "requirements.txt").read_text().splitlines()
 
 setup(
     name="hardocr",
-    version="0.1.5",
+    version="0.1.6",
     description="None",
     install_requires=install_requires,
 )
